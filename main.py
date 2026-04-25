df3748a041e0f501377c221a3b9e64eec393f7d0
David Ekeocha <davidekeocha36@gmail.com>
2026-04-25 01:04:29 +0100

    Implemented input loop with valdation and safe user input handling

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import turtle
+import pandas
+
+data = pandas.read_csv("50_states.csv")
+states = data["state"].tolist()
+
+user_states = []
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,4 +12,17 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+while True:
+    answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+
+    if answer_state:   # ← safety check
+        user_answer = answer_state.title()
+
+        if user_answer in states:
+            print("Correct")
+        
+
+
+
+
+
